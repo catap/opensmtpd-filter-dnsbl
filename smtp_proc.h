b110fcdbc8da12fed7f2ce0c23e96cbc72830a5d
martijn <martijn@108effe5-3eeb-f34f-8a8c-911c1f37f42a>
2019-03-29 07:27:14 +0000

    Add support for smtp-in link-disconnect reports

--- smtp_proc.h
+++ smtp_proc.h
@@ -12,6 +12,8 @@ struct inx_addr {
 
 int smtp_register_filter_connect(void (*cb)(char *, int, struct timespec *,
     char *, char *, uint64_t, uint64_t, char *, struct inx_addr *));
+int smtp_in_register_report_disconnect(void (*)(char *, int, struct timespec *,
+    char *, char *, uint64_t));
 void smtp_filter_proceed(uint64_t, uint64_t);
 void smtp_filter_reject(uint64_t, uint64_t, int, const char *, ...)
 	__attribute__((__format__ (printf, 4, 5)));
